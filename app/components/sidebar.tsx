1fcef5b844a5dad5b358abfcc9765a49fe450cfd
芦明宝 <lumingbao@lzfsd.com>
2023-04-23 14:50:19 +0800

    修改标题、默认发送按钮、无边框模式

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -90,7 +90,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>西岚的ChatGPT助理</div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
         </div>
